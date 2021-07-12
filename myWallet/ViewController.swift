71c540ce424f371954dafb5415306614e5eec1a2
David Avendaño <d.avendano.c@hotmail.com>
2021-07-11 22:19:25 -0500

    Initial Commit

---
+++ myWallet/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  myWallet
+//
+//  Created by David Avendaño on 11/07/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
